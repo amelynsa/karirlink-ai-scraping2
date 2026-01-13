1198d884d81fbb7ecfc5321042d231e1eb1aa086
rizaldinur <rnur71@gmail.com>
2026-01-13 09:35:28 +0700

    add: interface for gemini API response data

---
+++ types/interface.ts
@@ -0,0 +1,20 @@
+import { ApiError, GenerateContentResponseUsageMetadata } from "@google/genai";
+
+interface SuccessResponse {
+  success: true;
+  message: string;
+  data: {
+    content: string;
+    usage?: GenerateContentResponseUsageMetadata;
+  };
+}
+
+interface ErrorResponse {
+  success: false;
+  message: string;
+  data: {
+    error: ApiError;
+  };
+}
+
+export type ResponseData = SuccessResponse | ErrorResponse;
