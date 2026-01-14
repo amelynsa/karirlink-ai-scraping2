ca9f8dd43d35d521bc8c3a9cb8d6e7ca16a4a52f
rizaldinur <rnur71@gmail.com>
2026-01-14 11:26:17 +0700

    fix: add logic for handling pagination navigation, sttructure the return type, and add retries for page navigation with max limit

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,25 +1,135 @@
 import { readFile } from "fs/promises";
 import { Browser } from "puppeteer";
 import xlsx from "xlsx";
+import { getNextButton } from "../ai/ai.ts";
+import * as fs from "fs";
+import readline from "readline";
+import { isElementDisabled } from "../helpers/helpers.js";
+import { type Result } from "../types/interface.js";
+
+const MAX_RETRY = 3;
 
 export async function getDOMBody(
   browser: Browser,
-  url: string
-): Promise<string> {
+  url: string,
+  pageLimit?: number
+): Promise<Result> {
   const page = await browser.newPage();
+  let allBodyElements: string[] = [];
+  let usageToken: object[] = [];
+
   try {
-    const page = await browser.newPage();
+    for (let i = 0; i < MAX_RETRY; i++) {
+      try {
     await page.goto(url, {
-      waitUntil: "domcontentloaded",
-    });
+          waitUntil: "networkidle2",
+        });
+      } catch (error) {
+        if (i === MAX_RETRY) {
+          throw error;
+        }
+        continue;
+      }
+    }
 
-    const body = await page.$eval("body", (el) => el.innerHTML);
+    // const body = await page.$eval("body", (el) => el.innerHTML);
+    const isBodyReady = await page.waitForFunction(
+      () => document.body !== null
+    );
 
-    await page.close();
-    return body;
+    let body = "";
+    if (isBodyReady) {
+      body = await page.$eval("body", (el) => el.innerHTML);
+    }
+
+    // let nextPageNavigator;
+    let nextBtnSelector;
+    // const stream = fs.createWriteStream("./storage/test-get-all.jsonl", {
+    //   flags: "a",
+    // });
+    if (body) {
+      // console.log("Write first html body to stream...");
+      // stream.write(`${JSON.stringify({ htmlBody: body })}\n`);
+      allBodyElements.push(body);
+      const { success, data } = await getNextButton(body);
+      if (success) {
+        usageToken.push(data?.usage || {});
+        nextBtnSelector = JSON.parse(data?.content)?.btnIdentifier || "";
+        console.log("Next page selector: ", nextBtnSelector);
+      }
+    }
+
+    let pageCounter = 1;
+    while (true) {
+      if (!nextBtnSelector) {
+        break;
+      }
+      const nextPageNavigator = await page
+        .waitForSelector(nextBtnSelector)
+        .catch(() => null);
+
+      if (!nextPageNavigator) {
+        break;
+      }
+
+      // let isButton = await nextPageNavigator?.evaluate(
+      //   (el) => el.tagName === "BUTTON"
+      // );
+
+      // if (isButton) {
+      //   const isDisabled: boolean =
+      //     (await nextPageNavigator?.evaluate(
+      //       (button) => (button as HTMLButtonElement).disabled
+      //     )) || true;
+
+      //   if (isDisabled) {
+      //     break;
+      //   }
+      // }
+
+      const isDisabled = await isElementDisabled(nextPageNavigator);
+      if (isDisabled) break;
+
+      await Promise.all([
+        page.waitForNavigation().catch(() => null),
+        nextPageNavigator?.click(),
+      ]);
+
+      pageCounter++;
+      if (pageLimit && pageCounter > pageLimit) {
+        break;
+      }
+
+      const isNextBodyReady = await page.waitForFunction(
+        () => document.body !== null
+      );
+
+      let nextBody = "";
+      if (isNextBodyReady) {
+        nextBody = await page.$eval("body", (el) => el.innerHTML);
+      }
+
+      if (nextBody) {
+        // console.log("Write stream to file...");
+        // stream.write(`${JSON.stringify({ htmlBody: nextBody })}\n`);
+        allBodyElements.push(nextBody);
+        const { success, data } = await getNextButton(
+          nextBody,
+          nextBtnSelector
+        );
+
+        if (success) {
+          usageToken.push(data?.usage || {});
+          nextBtnSelector = JSON.parse(data?.content)?.btnIdentifier || "";
+          console.log("Next page selector: ", nextBtnSelector);
+        }
+      }
+    }
+
+    return { data: allBodyElements, usage: usageToken };
   } catch (error: any) {
-    console.error("\n", error.message);
-    return "";
+    console.error(error);
+    return { data: allBodyElements, usage: usageToken };
   } finally {
     await page.close();
   }
