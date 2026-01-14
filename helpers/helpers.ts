cbcee78c9cbd3c4dc7170e0b4868f053c68e0251
rizaldinur <rnur71@gmail.com>
2026-01-14 11:28:26 +0700

    add: safe parse json object and iselementdisabled func

--- helpers/helpers.ts
+++ helpers/helpers.ts
@@ -10,3 +10,24 @@ export function safeParseJSONArrayData(input?: string): any[] {
     return [];
   }
 }
+
+export function safeParseJSON<T>(input: string): T | null {
+  try {
+    return JSON.parse(input) as T;
+  } catch {
+    return null;
+  }
+}
+
+export async function isElementDisabled(handle: ElementHandle) {
+  return handle.evaluate((el) =>
+    el instanceof HTMLInputElement ||
+    el instanceof HTMLButtonElement ||
+    el instanceof HTMLSelectElement ||
+    el instanceof HTMLTextAreaElement ||
+    el instanceof HTMLOptionElement ||
+    el instanceof HTMLFieldSetElement
+      ? el.disabled
+      : false
+  );
+}
