ae44133c1e25b9d88c418c2cadf9e9ff5533dd4b
rizaldinur <rnur71@gmail.com>
2026-01-14 14:04:38 +0700

    fix: add logs for every action, handle flow for scrape and extracting data

--- index.ts
+++ index.ts
@@ -1,4 +1,3 @@
-import { writeFile, mkdir, readFile } from "fs/promises";
 import { extractData } from "./ai/ai.ts";
 import {
   getDOMBody,
@@ -29,41 +28,84 @@ async function main() {
   });
 
   try {
-    const rows = readSourcesFromExcel("./storage/source.xlsx");
+    console.log("Reading sources...");
+    const rows = readSourcesFromExcel("./storage/source-10.xlsx");
 
     let extractedData: Object[] = [];
     let usageData: any[] = [];
 
-    for (const [index, row] of rows.entries()) {
-      if (index === 5) break;
-      const body = await getDOMBody(browser, row.karirURL || "");
-      const { data, usage } = await extractData(body);
-      usageData.push(usage || {});
-      extractedData = [...extractedData, ...JSON.parse(data)];
+    console.log("Extracting data from sources...\n");
+    for (const row of rows) {
+      console.log(`Getting raw HTML body from: ${row.karirURL}...`);
+      const { data, usage } = await getDOMBody(browser, row.karirURL || "", 2);
+      console.log(`Writing usage log to write stream from getDOMBODY`);
+      if (usage.length > 0) {
+        usage.forEach((value) => {
+          streamUsageLog.write(`${JSON.stringify(value)}\n`);
+        });
+      }
+
+      console.log(
+        `AI is extracting and parsing data from HTML body of: ${row.karirURL}...`
+      );
+      for (const [index, raw] of data.entries()) {
+        const { success, message, data } = await extractData(raw);
+        await new Promise((resolve) => setTimeout(resolve, 15000));
+        if (success) {
+          usageData.push(usage || {});
+          extractedData.push(...safeParseJSONArrayData(data?.content));
+
+          if (data?.usage) {
+            console.log(`(${index + 1}) Writing usage log to write stream`);
+            streamUsageLog.write(`${JSON.stringify(data?.usage)}\n`);
+          }
+          if (
+            safeParseJSONArrayData(data?.content) instanceof Array &&
+            safeParseJSONArrayData(data?.content).length > 0
+          ) {
+            console.log(
+              `(${index + 1}) Writing extracted data to write stream`
+            );
+            JSON.parse(data?.content).forEach((value: any) => {
+              const writeResponse = {
+                success,
+                message,
+                data: value,
+              };
+
+              streamExtractedData.write(`${JSON.stringify(writeResponse)}\n`);
+            });
+          }
+        } else {
+          console.log("Writing extracted data to write stream");
+          const writeResponse = {
+            success,
+            message,
+            data: data?.error,
+          };
+          streamExtractedData.write(`${JSON.stringify(writeResponse)}\n`);
+        }
+      }
+      console.log(
+        `AI finished extracting and parsing from HTML body of: ${row.karirURL}\n`
+      );
     }
+    console.log("Finished extracting data from all sources");
+    console.log(`Total successful extracted data: ${extractedData.length}`);
 
-    await mkdir("./logs", { recursive: true });
-    await writeFile(
-      "./logs/usage-log.json",
-      JSON.stringify(usageData, null, 2)
-    );
-
-    await mkdir("./storage", { recursive: true });
-    await writeFile(
-      "./storage/test-result.json",
-      JSON.stringify(extractedData, null, 2)
-    );
-    await browser.close();
-    console.log("\nExtracting job listings done.");
-    console.log("\nCounting total extraction usage token...");
-    await sumTotalUsageToken();
-    process.exit(0);
+    console.log("Counting total extraction usage token...");
+    await sumTotalUsageToken("./logs/usage-log.jsonl");
+    console.log();
   } catch (error) {
-    console.error("\n", error);
+    console.error(error);
+    throw error;
+  } finally {
     await browser.close();
-    process.exit(1);
   }
 }
 
+console.time("Process finished in: ");
+console.log("Process starting...");
 await main();
-clearInterval(timer);
+// await sumTotalUsageToken("./logs/usage-log.jsonl");
+console.timeEnd("Process finished in: ");
