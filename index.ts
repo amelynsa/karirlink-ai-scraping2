f46e39cf6b53871da2406489a750c959301d46b2
amelynsa <Ameliayunisaa@gmail.com>
2026-01-20 14:10:28 +0700

    resolve konflik

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import { writeFile, mkdir, readFile } from "fs/promises";
 import { extractData } from "./ai/ai.ts";
 import {
   getDOMBody,
@@ -5,101 +6,51 @@ import {
   sumTotalUsageToken,
 } from "./utils/utils.ts";
 import puppeteer from "puppeteer";
-import * as fs from "fs";
-import { safeParseJSONArrayData } from "./helpers/helpers.ts";
 
-const LOG_FILE_PATH = "./logs/usage-log.jsonl";
-const RESULT_FILE_PATH = "./storage/test-result.jsonl";
+const startTime = process.hrtime.bigint();
+const timer = setInterval(() => {
+  const elapsedSec = Number(process.hrtime.bigint() - startTime) / 1e9;
+  const minutes = Math.floor(elapsedSec / 60);
+  const seconds = Math.floor(elapsedSec % 60);
+  process.stdout.write(
+    `\rElapsed: ${minutes}:${seconds.toString().padStart(2, "0")} (m:ss)`
+  );
+}, 1000); // Update every 1 second
 
 async function main() {
   const browser = await puppeteer.launch();
-
-  if (fs.existsSync(LOG_FILE_PATH)) {
-    fs.truncateSync(LOG_FILE_PATH, 0);
-  }
-  if (fs.existsSync(RESULT_FILE_PATH)) {
-    fs.truncateSync(RESULT_FILE_PATH, 0);
-  }
-  const streamUsageLog = fs.createWriteStream(LOG_FILE_PATH, {
-    flags: "a",
-  });
-  const streamExtractedData = fs.createWriteStream(RESULT_FILE_PATH, {
-    flags: "a",
-  });
-
   try {
-    console.log("Reading sources...");
-    const rows = readSourcesFromExcel("./storage/source-10.xlsx");
+    const rows = readSourcesFromExcel("./storage/source.xlsx");
 
-    let extractedData: Object[] = [];
+    let extractedData: any[] = [];
     let usageData: any[] = [];
 
-    console.log("Extracting data from sources...\n");
-    for (const row of rows) {
-      console.log(`Getting raw HTML body from: ${row.karirURL}...`);
-      const { data, usage } = await getDOMBody(browser, row.karirURL || "", 2);
-      console.log(`Writing usage log to write stream from getDOMBODY`);
-      if (usage.length > 0) {
-        usage.forEach((value) => {
-          streamUsageLog.write(`${JSON.stringify(value)}\n`);
-        });
-      }
-
-      console.log(
-        `AI is extracting and parsing data from HTML body of: ${row.karirURL}...`
-      );
-      for (const [index, raw] of data.entries()) {
-        const { success, message, data } = await extractData(raw);
-        await new Promise((resolve) => setTimeout(resolve, 15000));
-        if (success) {
-          usageData.push(usage || {});
-          extractedData.push(...safeParseJSONArrayData(data?.content));
-
-          if (data?.usage) {
-            console.log(`(${index + 1}) Writing usage log to write stream`);
-            streamUsageLog.write(`${JSON.stringify(data?.usage)}\n`);
-          }
-          if (
-            safeParseJSONArrayData(data?.content) instanceof Array &&
-            safeParseJSONArrayData(data?.content).length > 0
-          ) {
-            console.log(
-              `(${index + 1}) Writing extracted data to write stream`
-            );
-            JSON.parse(data?.content).forEach((value: any) => {
-              const writeResponse = {
-                success,
-                message,
-                data: value,
-              };
-
-              streamExtractedData.write(`${JSON.stringify(writeResponse)}\n`);
-            });
-          }
-        } else {
-          console.log("Writing extracted data to write stream");
-          const writeResponse = {
-            success,
-            message,
-            data: data?.error,
-          };
-          streamExtractedData.write(`${JSON.stringify(writeResponse)}\n`);
-        }
-      }
-      console.log(
-        `AI finished extracting and parsing from HTML body of: ${row.karirURL}\n`
-      );
+    for (const [index, row] of rows.entries()) {
+      if (index === 5) break;
+      const body = await getDOMBody(browser, row.karirURL || "");
+      const { data, usage } = await extractData(body);
+      usageData.push(usage || {});
+      extractedData = [...extractedData, ...JSON.parse(data)];
     }
-    console.log("Finished extracting data from all sources");
-    console.log(`Total successful extracted data: ${extractedData.length}`);
 
-    console.log("Counting total extraction usage token...");
-    await sumTotalUsageToken("./logs/usage-log.jsonl");
-    console.log();
+    await mkdir("./logs", { recursive: true });
+    await writeFile(
+      "./logs/usage-log.json",
+      JSON.stringify(usageData, null, 2)
+    );
+
+    await mkdir("./storage", { recursive: true });
+    await writeFile(
+      "./storage/test-result.json",
+      JSON.stringify(extractedData, null, 2)
+    );
+    await browser.close();
+    console.log("\nExtracting job listings done.");
+    console.log("\nCounting total extraction usage token...");
+    await sumTotalUsageToken();
+    process.exit(0);
   } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
+    console.error("\n", error);
     await browser.close();
   }
 }
@@ -107,5 +58,4 @@ async function main() {
 console.time("Process finished in: ");
 console.log("Process starting...");
 await main();
-// await sumTotalUsageToken("./logs/usage-log.jsonl");
-console.timeEnd("Process finished in: ");
+clearInterval(timer);
